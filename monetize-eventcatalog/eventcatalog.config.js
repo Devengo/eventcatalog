901b676de539af1af7da482143a01ad3bddf4bca
Robert Dahlborg <robert@dahlborg.com>
2023-08-08 13:31:54 +0200

    Initital commit

---
+++ monetize-eventcatalog/eventcatalog.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  title: 'EventCatalog',
+  tagline: 'Discover, Explore and Document your Event Driven Architectures',
+  organizationName: 'Your Company',
+  projectName: 'Event Catalog',
+  editUrl: 'https://github.com/Devengo/eventcatalog/edit/master',
+  trailingSlash: true,
+  primaryCTA: {
+    label: 'Explore Events',
+    href: '/events'
+  },
+  secondaryCTA: {
+    label: 'Getting Started',
+    href:"https://www.eventcatalog.dev/"
+  },
+  logo: {
+    alt: 'EventCatalog Logo',
+    // found in the public dir
+    src: 'logo.svg',
+  },
+  footerLinks: [
+    { label: 'Events', href: '/events' },
+    { label: 'Services', href: '/services' },
+    { label: 'Visualiser', href: '/visualiser' },
+    { label: '3D Node Graph', href: '/overview' },
+    { label: 'GitHub', href: 'https://github.com/boyney123/eventcatalog-demo/edit/master' }
+  ],
+  users: [
+    {
+      id: 'rdahlborg',
+      name: 'Robert Dahlborg',
+      avatarUrl: 'https://www.gravatar.com/avatar/52d26b2736d513a82d300c61e0e4bfa1?s=400',
+      role: 'Developer',
+    },
+    {
+      id: 'jdahlborg',
+      name: 'Johan Dahlborg',
+      avatarUrl: 'https://www.gravatar.com/avatar/bb86d65b4a9e510700b77291ac328beb?s=400',
+      role: 'Developer',
+    },
+  ],
+}
